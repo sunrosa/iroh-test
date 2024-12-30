12d818a617626bf28f186dc951d9c16978ad6a9d
Ibis Sunrosa <drsunrosa@gmail.com>
2024-12-30 04:00:19 +0000

    Receive from 2 clients

--- src/server/main.rs
+++ src/server/main.rs
@@ -2,6 +2,10 @@ use std::str::{from_utf8, FromStr};
 
 use anyhow::anyhow;
 use iroh::{Endpoint, SecretKey};
+use tokio::{
+    join, select,
+    sync::mpsc::{self, UnboundedSender},
+};
 
 fn main() -> anyhow::Result<()> {
     tokio::runtime::Builder::new_multi_thread()
@@ -21,14 +25,31 @@ async fn async_main() -> anyhow::Result<()> {
         .bind()
         .await?;
 
+    let (mpscsend, mut mpscrecv) = mpsc::unbounded_channel();
+
+    let recv1 = tokio::task::spawn(receiver(ep.clone(), mpscsend.clone()));
+    let recv2 = tokio::task::spawn(receiver(ep, mpscsend));
+
+    loop {
+        let msg = mpscrecv
+            .recv()
+            .await
+            .ok_or(anyhow!("message mpsc closed and empty"))?;
+        println!("{msg}");
+    }
+
+    join!(recv1, recv2);
+}
+
+async fn receiver(ep: Endpoint, sender: UnboundedSender<String>) -> anyhow::Result<()> {
     let conn = ep.accept().await.ok_or(anyhow!("err"))?.await?;
     println!("connection established with {:?}", conn.remote_address());
-
     let mut recv = conn.accept_uni().await?;
 
     loop {
         let mut buf: [u8; 1024] = [0; 1024];
         recv.read(&mut buf).await?;
-        println!("{}", from_utf8(&buf)?);
+
+        sender.send(format!("{}: {}", conn.stable_id(), from_utf8(&buf)?))?;
     }
 }
