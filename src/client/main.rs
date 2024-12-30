a41b0e844b5e3ae891bb77121d880d7dcc6c8955
Ibis Sunrosa <drsunrosa@gmail.com>
2024-12-30 02:56:08 +0000

    It works!

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,1 +1,34 @@
-fn main() {}
+use std::str::FromStr;
+
+use iroh_net::{key::PublicKey, Endpoint, NodeAddr};
+
+fn main() -> anyhow::Result<()> {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async_main())
+}
+
+async fn async_main() -> anyhow::Result<()> {
+    println!("enter address:");
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf)?;
+    let buf = buf.trim();
+
+    // let buf_decoded = base32::decode(Rfc4648Lower { padding: false }, &buf)
+    //     .ok_or(anyhow!("could not decode base32"))?;
+
+    let addr = NodeAddr::new(PublicKey::from_str(buf)?);
+    let ep = Endpoint::builder().discovery_n0().bind().await?;
+    let conn = ep.connect(addr, b"my-alpn").await?;
+    let mut send = conn.open_uni().await?;
+
+    loop {
+        let mut buf = String::new();
+        std::io::stdin().read_line(&mut buf)?;
+        let buf = buf.trim();
+
+        send.write_all(buf.as_bytes()).await?;
+    }
+}
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("pemis")
+use std::str::from_utf8;
+
+use anyhow::anyhow;
+use iroh_net::Endpoint;
+
+fn main() -> anyhow::Result<()> {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async_main())
+}
+
+async fn async_main() -> anyhow::Result<()> {
+    let ep = Endpoint::builder()
+        .alpns(vec![b"my-alpn".to_vec()])
+        .discovery_n0()
+        .bind()
+        .await?;
+
+    println!("node id: {}\nawaiting connection...", ep.node_id());
+
+    let conn = ep.accept().await.ok_or(anyhow!("err"))?.await?;
+    println!("connection established with {:?}", conn.remote_address());
+
+    let mut recv: iroh_net::endpoint::RecvStream = conn.accept_uni().await?;
+
+    loop {
+        let mut buf: [u8; 1024] = [0; 1024];
+        recv.read(&mut buf).await?;
+        println!("{}", from_utf8(&buf)?);
+    }
 }
